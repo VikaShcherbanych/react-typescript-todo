1484b0cddb4d04e08c50c192b7d52057e6cd6997
Vika Shcherbanych <vika.shcherbanych@dev.pro>
2022-02-28 01:26:36 +0200

    Todo List

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import TodosView from './components/TodoViews';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        TODO
+      </header>
+      <TodosView />
+    </div>
+  );
+}
+
+export default App;
